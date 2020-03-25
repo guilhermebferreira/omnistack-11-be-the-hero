4a2764265abad114b95a79a653b49a43b88a37c8
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-03-24 21:07:23 -0300

    [ADD] adicionando ESlint ao projeto

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require( "express" ),
 
-const app = express();
+    app = express();
 
-app.get('/', (request, response) => {
-    return response.json({
-        evento: 'Semana OmniStack 11.0',
-        aluno: 'Guilherme Ferreira'
-    })
-});
+app.get( "/", ( request, response ) => {
+    return response.json( {
+        "evento": "Semana OmniStack 11.0",
+        "aluno": "Guilherme Ferreira"
+    } );
+} );
 
-app.listen(3333);
+app.listen( 3333 );
