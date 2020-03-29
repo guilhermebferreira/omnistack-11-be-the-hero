80b0d74d4cf775b56affec99ca4e7b994d5d6dc0
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-03-28 23:04:04 -0300

    [UPDATE] implementando a funcionalidade de logon

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,22 +1,50 @@
-import React from "react";
-import {Link} from 'react-router-dom'
+import React, {useState} from "react";
+import {Link, useHistory} from 'react-router-dom'
 import {FiLogIn} from 'react-icons/fi';
+import api from '../../services/api';
 import './styles.css';
-import heroesImage from  '../../assets/heroes.png'
-import logoImage from  '../../assets/logo.svg'
+import heroesImage from '../../assets/heroes.png'
+import logoImage from '../../assets/logo.svg'
 
 export default function Logon() {
-    return(
+    const [id, setId] = useState('');
+
+    const history = useHistory();
+    async function handleLogin(e) {
+        e.preventDefault();
+        console.log('login', id);
+        try {
+            const response = await api.post('sessions', {id});
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName',response.data.name );
+            //
+            history.push('/profile');
+        } catch (e) {
+            alert('ONG não localizada, tente novamente.')
+        }
+
+    }
+
+    return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImage} alt="Be The Hero"/>
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Sua ID"/>
-                    <button className="button" type="submit">Entrar</button>
+                    <input placeholder="Sua ID"
+                           value={id}
+                           onChange={(e) => {
+                               setId(e.target.value);
+                           }}
+                    />
+                    <button
+                        className="button"
+                        type="submit"
+                    >Entrar
+                    </button>
 
-                    <Link className="back-link" to="/register" >
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#e02041"/>
                         Não tenho cadastro
                     </Link>
