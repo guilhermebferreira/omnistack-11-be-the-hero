fe47a4bd366c2bbb46223f091167962645df97ce
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-03-28 23:46:34 -0300

    [UPDATE] implementando a função de criar um novo incident

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,29 +1,75 @@
-import React from "react";
+import React, {useState} from "react";
 
 import './styles.css';
 import {Link, useHistory} from 'react-router-dom'
+
+import api from '../../services/api';
 import {FiArrowLeft} from 'react-icons/fi';
-import logoImage from  '../../assets/logo.svg'
+import logoImage from '../../assets/logo.svg'
 
 export default function NewIncident() {
-    return (  <div className="new-incident-container">
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+    const ongId = localStorage.getItem('ongId');
+
+    const history = useHistory();
+
+    async function handleNewIncident(e) {
+        e.preventDefault();
+        const data = {
+            title,
+            description,
+            value,
+        };
+
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId
+                }
+            }).then(() => {
+                history.push('/profile');
+            })
+        } catch (e) {
+            alert(`Erro ao cadastrar caso (${e.message})`)
+        }
+
+    }
+
+    return (<div className="new-incident-container">
         <div className="content">
             <section>
                 <img src={logoImage} alt="Be The Hero"/>
 
                 <h1>Cadastrar novo caso</h1>
                 <p>Descreva o caso detalhadamente para encontrar um heroí para resolver isso.</p>
-                <Link className="back-link" to="/profile" >
+                <Link className="back-link" to="/profile">
                     <FiArrowLeft size={16} color="#e02041"/>
                     Voltar para home
                 </Link>
             </section>
-            <form >
-                <input type="text" placeholder="Titulo do caso"/>
-                <textarea placeholder="Descrição do caso"/>
-                <input type="text" placeholder="Valor em reais"/>
+            <form onSubmit={handleNewIncident}>
+                <input
+                    type="text"
+                    placeholder="Titulo do caso"
+                    value={title}
+                    onChange={e=>setTitle(e.target.value)}
+                />
+                <textarea
+                    placeholder="Descrição do caso"
+                    value={description}
+                    onChange={e=>setDescription(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Valor em reais"
+                    value={value}
+                    onChange={e=>setValue(e.target.value)}
+                />
 
-                <button className="button" type="submit" >Cadastrar</button>
+                <button className="button" type="submit">Cadastrar</button>
 
             </form>
         </div>
