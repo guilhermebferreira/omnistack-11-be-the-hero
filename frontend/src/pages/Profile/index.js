f08bc449b1639f1475fe5fe026165df273efc206
guilhermebferreira <guilherme@mail.uft.edu.br>
2020-03-28 23:45:36 -0300

    [UPDATE] carregando itens da tela profile, e implementando função delete

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 import {FiPower, FiTrash2} from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -13,6 +13,7 @@ export default function Profile() {
     const ongId = localStorage.getItem('ongId');
     const [incidents, setIncidentes] = useState([]);
 
+    const history = useHistory();
     useEffect(
         () => {
             loadIncidents()
@@ -20,7 +21,7 @@ export default function Profile() {
         [ongId]
     );
 
-    function loadIncidents(){
+    function loadIncidents() {
         api.get('profile',
             {
                 headers: {
@@ -30,17 +31,22 @@ export default function Profile() {
             setIncidentes(response.data);
         });
     }
-    
+
     async function handleDelete(id) {
         try {
-            await api.delete(`incident/${id}`,  {headers:{Authorization: ongId}})
-                .then(()=>{
+            await api.delete(`incident/${id}`, {headers: {Authorization: ongId}})
+                .then(() => {
                     //loadIncidents();
                     setIncidentes(incidents.filter(incident => incident.id !== id));
                 });
-        }catch (e) {
+        } catch (e) {
             alert(`Erro ao deletar caso (${e.message})`);
         }
+    }
+
+    function handleLogout() {
+        localStorage.clear();
+        history.push('/');
     }
 
     return (
@@ -50,7 +56,7 @@ export default function Profile() {
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incident/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button type="button" onClick={handleLogout}>
                     <FiPower size={18} color="#e02041"/>
                 </button>
             </header>
@@ -69,7 +75,9 @@ export default function Profile() {
                         <strong>Valor:</strong>
                         <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value)}</p>
 
-                        <button type="button" onClick={()=>{handleDelete(incident.id)}}>
+                        <button type="button" onClick={() => {
+                            handleDelete(incident.id)
+                        }}>
                             <FiTrash2 size={20} color="#a8a8b3"/>
                         </button>
                     </li>
